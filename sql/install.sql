efd8084ba384dd4b0a9b44dd315764a2f0bacfbc
lumian <lum@it-35.ru>
2020-11-16 14:58:36 +0300

    Revert "Issue #25 (SQL dump)"
    This reverts commit 4f89bec2caa88d1f30e9c3946b6985ffb097119f.

--- sql/install.sql
+++ sql/install.sql
@@ -2,7 +2,7 @@
 -- Хост:                         127.0.0.1
 -- Версия сервера:               10.3.25-MariaDB-0+deb10u1 - Debian 10
 -- Операционная система:         debian-linux-gnu
--- HeidiSQL Версия:              11.1.0.6116
+-- HeidiSQL Версия:              11.0.0.6100
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -10,7 +10,6 @@
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
 -- Дамп структуры базы данных grcentral_base
@@ -95,16 +94,6 @@ CREATE TABLE IF NOT EXISTS `settings_models_group` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
   `params_group_id` int(11) DEFAULT NULL,
-  `params_conf_acc_atatus` varchar(50) DEFAULT NULL,
-  `params_conf_acc_name` varchar(50) DEFAULT NULL,
-  `params_conf_srv_main` varchar(50) DEFAULT NULL,
-  `params_conf_srv_reserve` varchar(50) DEFAULT NULL,
-  `params_conf_sip_userid` varchar(50) DEFAULT NULL,
-  `params_conf_sip_authid` varchar(50) DEFAULT NULL,
-  `params_conf_sip_passwd` varchar(50) DEFAULT NULL,
-  `params_conf_show_name` varchar(50) DEFAULT NULL,
-  `params_conf_acc_display` varchar(50) DEFAULT NULL,
-  `params_conf_voicemail` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -149,4 +138,3 @@ CREATE TABLE IF NOT EXISTS `settings_system` (
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
