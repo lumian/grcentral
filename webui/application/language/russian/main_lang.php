bd5ebe33ac05f27ecabf1e25d4b968a516784719
lumian <lum@it-35.ru>
2020-11-25 10:52:27 +0300

    Add link to help pages (Issue #21)

--- webui/application/language/russian/main_lang.php
+++ webui/application/language/russian/main_lang.php
@@ -58,5 +58,5 @@ $lang['main_helpurl_settings_params']			= "https://github.com/lumian/grcentral/w
 $lang['main_helpurl_settings_servers']			= "https://github.com/lumian/grcentral/wiki/VoIP-%D1%81%D0%B5%D1%80%D0%B2%D0%B5%D1%80%D1%8B";
 $lang['main_helpurl_devices']					= "https://github.com/lumian/grcentral/wiki/%D0%A3%D1%81%D1%82%D1%80%D0%BE%D0%B9%D1%81%D1%82%D0%B2%D0%B0";
 $lang['main_helpurl_devices_accounts']			= "https://github.com/lumian/grcentral/wiki/SIP-%D0%B0%D0%BA%D0%BA%D0%B0%D1%83%D0%BD%D1%82%D1%8B";
-$lang['main_helpurl_settings_system']			= "#";
-$lang['main_helpurl_phonebook_abonents']		= "#";
+$lang['main_helpurl_settings_system']			= "https://github.com/lumian/grcentral/wiki/%D0%A1%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%BD%D1%8B%D0%B5-%D0%BD%D0%B0%D1%81%D1%82%D1%80%D0%BE%D0%B9%D0%BA%D0%B8";
+$lang['main_helpurl_phonebook_abonents']		= "https://github.com/lumian/grcentral/wiki/%D0%A1%D0%BF%D1%80%D0%B0%D0%B2%D0%BE%D1%87%D0%BD%D0%B8%D0%BA";
